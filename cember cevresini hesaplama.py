e4f8f585f55fec3ee71fdfe56307d502f8ec4a66
goofyspark <75097234+goofyspark@users.noreply.github.com>
2020-12-06 19:00:42 +0300

    Add files via upload

---
+++ cember cevresini hesaplama.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+pi_sayisi = 3.14
+
+
+# In[2]:
+
+
+yaricap = 2
+
+
+# In[3]:
+
+
+cembercevre = yaricap*pi_sayisi*2
+
+
+# In[4]:
+
+
+cembercevre
+
+
+# Pi sayısını 3.14 olarak atadıktan sonra yarıçap değişkenimizi yazıyoruz (hesaplamak istediğimiz çemberin yarıçapı)
+# daha sonra çember formülünü sayısallıktan çıkarıp önceden atadığımız değişkenlere göre yazıyoruz.Burada bir örnek yaptım ve yarıçapı 2 olarak aldım.İlk kodum.
+# 
